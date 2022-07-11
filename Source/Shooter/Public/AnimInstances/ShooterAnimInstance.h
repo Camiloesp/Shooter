dc828c47885e7d3eeaa1691a4587907d46831a05
Camilo Espinosa <camesp96@gmail.com>
2022-07-10 22:28:54 -0600

    Animation Class and BP

---
+++ Source/Shooter/Public/AnimInstances/ShooterAnimInstance.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ShooterAnimInstance.generated.h"
+
+
+
+class AShooterCharacter;
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API UShooterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	/* 
+	* This will behave like a Tick function. 
+	* The AnimInstance does contain a function that sort of behaves like tick,
+	* and its called NativeUpdateAnimation
+	*/
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+
+	// Beginplay for anim instance.
+	virtual void NativeInitializeAnimation() override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	AShooterCharacter* ShooterCharacter;
+
+	/* Speed of the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	/* Is the character in the Air? */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	/* Is the character moving? */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating;
+
+};
